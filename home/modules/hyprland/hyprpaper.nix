02af0c6a392c771e6036a0b5a1cbdb829a0dd029
nboj <bud@example.com>
2025-07-27 00:47:58 -0400

    u

--- home/modules/hyprland/hyprpaper.nix
+++ home/modules/hyprland/hyprpaper.nix
@@ -1,16 +1,21 @@
-{ ... }:
-{
-	services.hyprpaper = {
-		enable = true;
-		settings = {
-			ipc = "off";
-			splash = false;
-			preload = [
-				"~/Pictures/wallpaper.jpeg"
-			];
-			wallpaper = [
-				", ~/Pictures/wallpaper.jpeg"
-			];
-		};
-	};
+{ config, ... }:
+
+let
+  wallpaper = ./wallpaper.jpeg;  # Path relative to your flake or config file
+in {
+  xdg.configFile."hypr/wallpaper.jpeg".source = wallpaper;
+
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "off";
+      splash = false;
+      preload = [
+        "${config.xdg.configHome}/hypr/wallpaper.jpeg"
+      ];
+      wallpaper = [
+        ", ${config.xdg.configHome}/hypr/wallpaper.jpeg"
+      ];
+    };
+  };
 }
