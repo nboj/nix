857dc17bbd99126d721ef6f2798934d491b0d774
nboj <bud@example.com>
2025-07-26 20:30:27 -0400

    u

--- modules/thunar.nix
+++ modules/thunar.nix
@@ -2,12 +2,16 @@
 {
   programs.thunar = {
     enable = true;
+	plugins = with pkgs.xfce; [
+		thunar-archive-plugin
+		thunar-volman
+	];
   };
   programs.xfconf.enable = true;
 
   services.tumbler.enable = true;
+  services.gvfs.enable = true;
   environment.systemPackages = with pkgs; [
-    gvfs
     imv
     vlc
   ];
